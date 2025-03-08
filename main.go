9cbcdd5d2cd1ba6d5b16243e2960a44115fc56ee
Christian <minisirhc@gmail.com>
2025-03-07 22:08:55 -0600

    [fix] cors

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"time"
-	"github.com/gin-contrib/cors"
+	"log"
 	orders "order_Event_Driven/src/orders/infraestructure"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	r:= gin.Default()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error al cargar el archivo .env")
+	}
+
+	allowOrigins := os.Getenv("ALLOW_ORIGINS")
+	originsList := strings.Split(allowOrigins, ",")
+	log.Println("ALLOW_ORIGINS:", allowOrigins)
+	for _, origin := range originsList {
+		if !strings.HasPrefix(origin, "http://") && !strings.HasPrefix(origin, "https://") {
+			log.Fatalf("Invalid origin: %s. Origins must start with http:// or https://", origin)
+		}
+	}
+	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
+		AllowOrigins:     originsList,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
